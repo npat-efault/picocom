96ca9260cdf22b6c4b4e88d245f6f5b0b9d96410
Nick Patavalis <npat@efault.net>
2015-08-15 16:14:33 +0300

    Minor refactor. Remove command processing code from loop()

--- picocom.c
+++ picocom.c
@@ -89,9 +89,6 @@ const char *flow_str[] = {
 #define KEY_RECEIVE '\x12' /* C-r: receive file */
 #define KEY_BREAK   '\x1c' /* C-\: break */
 
-#define STO STDOUT_FILENO
-#define STI STDIN_FILENO
-
 /**********************************************************************/
 
 /* implemented caracter mappings */
@@ -200,7 +197,19 @@ struct {
 	.emap = M_E_DFL
 };
 
+#define STO STDOUT_FILENO
+#define STI STDIN_FILENO
+
 int tty_fd;
+
+#ifndef TTY_Q_SZ
+#define TTY_Q_SZ 256
+#endif
+
+struct tty_q {
+	int len;
+	unsigned char buff[TTY_Q_SZ];
+} tty_q;
 
 /**********************************************************************/
 
@@ -352,6 +361,8 @@ fatal (const char *format, ...)
 	
 	exit(EXIT_FAILURE);
 }
+
+/**********************************************************************/
 
 #ifndef LINENOISE
 
@@ -509,6 +520,8 @@ read_receive_filename (void)
 }
 
 #endif /* of ifndef LINENOISE */
+
+/**********************************************************************/
 
 /* maximum number of chars that can replace a single characted
    due to mapping */
@@ -796,14 +809,184 @@ run_cmd(int fd, ...)
 
 /**********************************************************************/
 
-#ifndef TTY_Q_SZ
-#define TTY_Q_SZ 256
+/* Process command key. Returns non-zero if command results in picocom
+   exit, zero otherwise. */
+int
+do_command (unsigned char c)
+{
+	static int dtr_up = 0;
+	int newbaud, newflow, newparity, newbits;
+#ifndef LINENOISE
+	char fname[128];
+#else
+	char *fname;
 #endif
+	int r;
 
-struct tty_q {
-	int len;
-	unsigned char buff[TTY_Q_SZ];
-} tty_q;
+	switch (c) {
+	case KEY_EXIT:
+		return 1;
+	case KEY_QUIT:
+		term_set_hupcl(tty_fd, 0);
+		term_flush(tty_fd);
+		term_apply(tty_fd);
+		term_erase(tty_fd);
+		return 1;
+	case KEY_STATUS:
+		show_status(dtr_up);
+		break;
+	case KEY_PULSE:
+		fd_printf(STO, "\r\n*** pulse DTR ***\r\n");
+		if ( term_pulse_dtr(tty_fd) < 0 )
+			fd_printf(STO, "*** FAILED\r\n");
+		break;
+	case KEY_TOGGLE:
+		if ( dtr_up )
+			r = term_lower_dtr(tty_fd);
+		else
+			r = term_raise_dtr(tty_fd);
+		if ( r >= 0 ) dtr_up = ! dtr_up;
+		fd_printf(STO, "\r\n*** DTR: %s ***\r\n", 
+				  dtr_up ? "up" : "down");
+		break;
+	case KEY_BAUD_UP:
+	case KEY_BAUD_DN:
+		if (c == KEY_BAUD_UP)
+			opts.baud = baud_up(opts.baud);
+		else 
+			opts.baud = baud_down(opts.baud);
+		term_set_baudrate(tty_fd, opts.baud);
+		tty_q.len = 0; term_flush(tty_fd);
+		term_apply(tty_fd);
+		newbaud = term_get_baudrate(tty_fd, NULL);
+		if ( opts.baud != newbaud ) {
+			fd_printf(STO, "\r\n*** baud: %d (%d) ***\r\n", 
+					  opts.baud, newbaud);
+		} else {
+			fd_printf(STO, "\r\n*** baud: %d ***\r\n", opts.baud);
+		}
+		break;
+	case KEY_FLOW:
+		opts.flow = flow_next(opts.flow);
+		term_set_flowcntrl(tty_fd, opts.flow);
+		tty_q.len = 0; term_flush(tty_fd);
+		term_apply(tty_fd);
+		newflow = term_get_flowcntrl(tty_fd);
+		if ( opts.flow != newflow ) {
+			fd_printf(STO, "\r\n*** flow: %s (%s) ***\r\n", 
+					  flow_str[opts.flow], flow_str[newflow]);
+		} else {
+			fd_printf(STO, "\r\n*** flow: %s ***\r\n", 
+					  flow_str[opts.flow]);
+		}
+		break;
+	case KEY_PARITY:
+		opts.parity = parity_next(opts.parity);
+		term_set_parity(tty_fd, opts.parity);
+		tty_q.len = 0; term_flush(tty_fd);
+		term_apply(tty_fd);
+		newparity = term_get_parity(tty_fd);
+		if (opts.parity != newparity ) {
+			fd_printf(STO, "\r\n*** parity: %s (%s) ***\r\n",
+					  parity_str[opts.parity], 
+					  parity_str[newparity]);
+		} else {
+			fd_printf(STO, "\r\n*** parity: %s ***\r\n", 
+					  parity_str[opts.parity]);
+		}
+		break;
+	case KEY_BITS:
+		opts.databits = bits_next(opts.databits);
+		term_set_databits(tty_fd, opts.databits);
+		tty_q.len = 0; term_flush(tty_fd);
+		term_apply(tty_fd);
+		newbits = term_get_databits(tty_fd);
+		if (opts.databits != newbits ) {
+			fd_printf(STO, "\r\n*** databits: %d (%d) ***\r\n",
+					  opts.databits, newbits);
+		} else {
+			fd_printf(STO, "\r\n*** databits: %d ***\r\n", 
+					  opts.databits);
+		}
+		break;
+	case KEY_LECHO:
+		opts.lecho = ! opts.lecho;
+		fd_printf(STO, "\r\n*** local echo: %s ***\r\n", 
+				  opts.lecho ? "yes" : "no");
+		break;
+	case KEY_SEND:
+		if ( opts.send_cmd[0] == '\0' ) {
+			fd_printf(STO, "\r\n*** command disabled ***\r\n");
+			break;
+		}
+#ifndef LINENOISE
+		fd_printf(STO, "\r\n*** file: ");
+		r = fd_readline(STI, STO, fname, sizeof(fname));
+		fd_printf(STO, "\r\n");
+		if ( r <= -1 ) {
+			if ( errno == EINTR ) {
+				fd_printf(STO, "Cannot read filename!\r\n");
+				break;
+			} else {
+				fatal("cannot read filename: %s", strerror(errno));
+			}
+		}
+		run_cmd(tty_fd, opts.send_cmd, fname, NULL);
+#else
+		fname = read_send_filename();
+		if (fname == NULL) {
+			fd_printf(STO, "Cannot read filename!\r\n");
+			break;
+		}
+		run_cmd(tty_fd, opts.send_cmd, fname, NULL);
+		free(fname);
+#endif
+		break;
+	case KEY_RECEIVE:
+		if ( opts.receive_cmd[0] == '\0' ) {
+			fd_printf(STO, "\r\n*** command disabled ***\r\n");
+			break;
+		}
+#ifndef LINENOISE
+		fd_printf(STO, "*** file: ");
+		r = fd_readline(STI, STO, fname, sizeof(fname));
+		fd_printf(STO, "\r\n");
+
+		if ( r <= -1 ) {
+			if ( errno == EINTR ) {
+				fd_printf(STO, "Cannot read filename!\r\n");
+				break;
+			} else {
+				fatal("cannot read filename: %s", strerror(errno));
+			}
+		}
+		if ( fname[0] )
+			run_cmd(tty_fd, opts.receive_cmd, fname, NULL);
+		else
+			run_cmd(tty_fd, opts.receive_cmd, NULL);
+#else
+		fname = read_receive_filename();
+		if (fname == NULL) {
+			fd_printf(STO, "Cannot read filename!\r\n");
+			break;
+		}
+		if ( fname[0] )
+			run_cmd(tty_fd, opts.receive_cmd, fname, NULL);
+		else
+			run_cmd(tty_fd, opts.receive_cmd, NULL);
+		free(fname);
+#endif
+		break;
+	case KEY_BREAK:
+		term_break(tty_fd);
+		fd_printf(STO, "\r\n*** break sent ***\r\n");
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
 
 /**********************************************************************/
 
@@ -814,21 +997,12 @@ loop(void)
 		ST_COMMAND,
 		ST_TRANSPARENT
 	} state;
-	int dtr_up;
 	fd_set rdset, wrset;
-	int newbaud, newflow, newparity, newbits;
-#ifndef LINENOISE
-	char fname[128];
-#else
-	char *fname;
-#endif
-	int r, n;
+	int n;
 	unsigned char c;
-
 
 	tty_q.len = 0;
 	state = ST_TRANSPARENT;
-	dtr_up = 0;
 
 	for (;;) {
 		FD_ZERO(&rdset);
@@ -858,10 +1032,8 @@ loop(void)
 			}
 
 			switch (state) {
-
 			case ST_COMMAND:
 				if ( c == opts.escape ) {
-					state = ST_TRANSPARENT;
 					/* pass the escape character down */
 					if (tty_q.len + M_MAXMAP <= TTY_Q_SZ) {
 						n = do_map((char *)tty_q.buff + tty_q.len, 
@@ -869,174 +1041,16 @@ loop(void)
 						tty_q.len += n;
 						if ( opts.lecho ) 
 							map_and_write(STO, opts.emap, c);
-					} else 
+					} else {
 						fd_printf(STO, "\x07");
-					break;
+					}
+				} else {
+					/* process command key */
+					if ( do_command(c) )
+						return;
 				}
 				state = ST_TRANSPARENT;
-				switch (c) {
-				case KEY_EXIT:
-					return;
-				case KEY_QUIT:
-					term_set_hupcl(tty_fd, 0);
-					term_flush(tty_fd);
-					term_apply(tty_fd);
-					term_erase(tty_fd);
-					return;
-				case KEY_STATUS:
-					show_status(dtr_up);
-					break;
-				case KEY_PULSE:
-					fd_printf(STO, "\r\n*** pulse DTR ***\r\n");
-					if ( term_pulse_dtr(tty_fd) < 0 )
-						fd_printf(STO, "*** FAILED\r\n");
-					break;
-				case KEY_TOGGLE:
-					if ( dtr_up )
-						r = term_lower_dtr(tty_fd);
-					else
-						r = term_raise_dtr(tty_fd);
-					if ( r >= 0 ) dtr_up = ! dtr_up;
-					fd_printf(STO, "\r\n*** DTR: %s ***\r\n", 
-							  dtr_up ? "up" : "down");
-					break;
-				case KEY_BAUD_UP:
-				case KEY_BAUD_DN:
-					if (c == KEY_BAUD_UP)
-						opts.baud = baud_up(opts.baud);
-					else 
-						opts.baud = baud_down(opts.baud);
-					term_set_baudrate(tty_fd, opts.baud);
-					tty_q.len = 0; term_flush(tty_fd);
-					term_apply(tty_fd);
-					newbaud = term_get_baudrate(tty_fd, NULL);
-					if ( opts.baud != newbaud ) {
-						fd_printf(STO, "\r\n*** baud: %d (%d) ***\r\n", 
-								  opts.baud, newbaud);
-					} else {
-						fd_printf(STO, "\r\n*** baud: %d ***\r\n", opts.baud);
-					}
-					break;
-				case KEY_FLOW:
-					opts.flow = flow_next(opts.flow);
-					term_set_flowcntrl(tty_fd, opts.flow);
-					tty_q.len = 0; term_flush(tty_fd);
-					term_apply(tty_fd);
-					newflow = term_get_flowcntrl(tty_fd);
-					if ( opts.flow != newflow ) {
-						fd_printf(STO, "\r\n*** flow: %s (%s) ***\r\n", 
-								  flow_str[opts.flow], flow_str[newflow]);
-					} else {
-						fd_printf(STO, "\r\n*** flow: %s ***\r\n", 
-								  flow_str[opts.flow]);
-					}
-					break;
-				case KEY_PARITY:
-					opts.parity = parity_next(opts.parity);
-					term_set_parity(tty_fd, opts.parity);
-					tty_q.len = 0; term_flush(tty_fd);
-					term_apply(tty_fd);
-					newparity = term_get_parity(tty_fd);
-					if (opts.parity != newparity ) {
-						fd_printf(STO, "\r\n*** parity: %s (%s) ***\r\n",
-								  parity_str[opts.parity], 
-								  parity_str[newparity]);
-					} else {
-						fd_printf(STO, "\r\n*** parity: %s ***\r\n", 
-								  parity_str[opts.parity]);
-					}
-					break;
-				case KEY_BITS:
-					opts.databits = bits_next(opts.databits);
-					term_set_databits(tty_fd, opts.databits);
-					tty_q.len = 0; term_flush(tty_fd);
-					term_apply(tty_fd);
-					newbits = term_get_databits(tty_fd);
-					if (opts.databits != newbits ) {
-						fd_printf(STO, "\r\n*** databits: %d (%d) ***\r\n",
-								  opts.databits, newbits);
-					} else {
-						fd_printf(STO, "\r\n*** databits: %d ***\r\n", 
-								  opts.databits);
-					}
-					break;
-				case KEY_LECHO:
-					opts.lecho = ! opts.lecho;
-					fd_printf(STO, "\r\n*** local echo: %s ***\r\n", 
-							  opts.lecho ? "yes" : "no");
-					break;
-				case KEY_SEND:
-					if ( opts.send_cmd[0] == '\0' ) {
-						fd_printf(STO, "\r\n*** command disabled ***\r\n");
-						break;
-					}
-#ifndef LINENOISE
-					fd_printf(STO, "\r\n*** file: ");
-					r = fd_readline(STI, STO, fname, sizeof(fname));
-					fd_printf(STO, "\r\n");
-					if ( r <= -1 ) {
-						if ( errno == EINTR ) {
-							fd_printf(STO, "Cannot read filename!\r\n");
-							break;
-						} else {
-							fatal("cannot read filename: %s", strerror(errno));
-						}
-					}
-					run_cmd(tty_fd, opts.send_cmd, fname, NULL);
-#else
-					fname = read_send_filename();
-					if (fname == NULL) {
-						fd_printf(STO, "Cannot read filename!\r\n");
-						break;
-					}
-					run_cmd(tty_fd, opts.send_cmd, fname, NULL);
-					free(fname);
-#endif
-					break;
-				case KEY_RECEIVE:
-					if ( opts.receive_cmd[0] == '\0' ) {
-						fd_printf(STO, "\r\n*** command disabled ***\r\n");
-						break;
-					}
-#ifndef LINENOISE
-					fd_printf(STO, "*** file: ");
-					r = fd_readline(STI, STO, fname, sizeof(fname));
-					fd_printf(STO, "\r\n");
-
-					if ( r <= -1 ) {
-						if ( errno == EINTR ) {
-							fd_printf(STO, "Cannot read filename!\r\n");
-							break;
-						} else {
-							fatal("cannot read filename: %s", strerror(errno));
-						}
-					}
-					if ( fname[0] )
-						run_cmd(tty_fd, opts.receive_cmd, fname, NULL);
-					else
-						run_cmd(tty_fd, opts.receive_cmd, NULL);
-#else
-					fname = read_receive_filename();
-					if (fname == NULL) {
-						fd_printf(STO, "Cannot read filename!\r\n");
-						break;
-					}
-					if ( fname[0] )
-						run_cmd(tty_fd, opts.receive_cmd, fname, NULL);
-					else
-						run_cmd(tty_fd, opts.receive_cmd, NULL);
-					free(fname);
-#endif
-					break;
-				case KEY_BREAK:
-					term_break(tty_fd);
-					fd_printf(STO, "\r\n*** break sent ***\r\n");
-					break;
-				default:
-					break;
-				}
 				break;
-
 			case ST_TRANSPARENT:
 				if ( c == opts.escape ) {
 					state = ST_COMMAND;
@@ -1051,7 +1065,6 @@ loop(void)
 						fd_printf(STO, "\x07");
 				}
 				break;
-
 			default:
 				assert(0);
 				break;
